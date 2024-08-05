686881a3b5669aafd62235b29a5900d5275b836e
skyler-code <skylerh15@github.com>
2024-08-05 00:43:33 -0500

    GUI start for 3rd cap

--- ReforgeLite.lua
+++ ReforgeLite.lua
@@ -87,6 +87,17 @@ local DefaultDBProfile = {
           preset = 1
         }
       }
+    },
+    {
+      stat = 0,
+      points = {
+        {
+          method = 1,
+          value = 0,
+          after = 0,
+          preset = 1
+        }
+      }
     }
   },
   itemsLocked = {},
@@ -988,7 +999,18 @@ function ReforgeLite:CreateItemTable ()
 end
 
 function ReforgeLite:AddCapPoint (i, loading)
-  local row = (loading or #self.pdb.caps[i].points + 1) + (i == 1 and 1 or #self.pdb.caps[1].points + 2)
+  local row = (loading or #self.pdb.caps[i].points + 1)-- + (i == 1 and 1 or #self.pdb.caps[i-1].points + 2)
+  if i == 1 then
+    row = row + 1
+  else
+    local subRows = 0
+    for k, v in ipairs(self.pdb.caps) do
+      if k < i then
+        subRows = subRows + #v.points
+      end
+    end
+    row = row + i + subRows
+  end
   local point = (loading or #self.pdb.caps[i].points + 1)
   self.statCaps:AddRow (row)
 
@@ -1050,7 +1072,14 @@ function ReforgeLite:AddCapPoint (i, loading)
   self.statCaps:OnUpdateFix()
 end
 function ReforgeLite:RemoveCapPoint (i, point, loading)
-  local row = #self.pdb.caps[1].points + (i == 1 and 1 or #self.pdb.caps[2].points + 2)
+  local subRows = 1
+  for k, v in ipairs(self.pdb.caps) do
+    if k < i then
+      subRows = subRows + #v.points
+    end
+  end
+  local row = i + subRows
+
   tremove (self.pdb.caps[i].points, point)
   self.statCaps:DeleteRow (row)
   if not loading then
@@ -1122,7 +1151,7 @@ function ReforgeLite:SetStatWeights (weights, caps)
     end
   end
   if caps then
-    for i = 1, 2 do
+    for i = 1, 3 do
       local count = 0
       if caps[i] then
         count = #caps[i].points
@@ -1371,7 +1400,7 @@ function ReforgeLite:CreateOptionList ()
   self.statWeightsCategory:AddFrame (self.statWeights)
   self.statWeights:SetRowHeight (self.db.itemSize + 2)
 
-  self.statCaps = GUI:CreateTable (2, 4, nil, self.db.itemSize + 2)
+  self.statCaps = GUI:CreateTable (3, 4, nil, self.db.itemSize + 2)
   self.statWeightsCategory:AddFrame (self.statCaps)
   self:SetAnchor (self.statCaps, "TOPLEFT", self.statWeights, "BOTTOMLEFT", 0, -10)
   self.statCaps:SetPoint ("RIGHT", -5, 0)
@@ -1383,7 +1412,7 @@ function ReforgeLite:CreateOptionList ()
   for i, v in ipairs (self.itemStats) do
     tinsert (statList, {value = i, name = v.long})
   end
-  for i = 1, 2 do
+  for i = 1, 3 do
     self.statCaps[i] = {}
     self.statCaps[i].stat = GUI:CreateDropdown (self.statCaps, statList, {
       default = self.pdb.caps[i].stat,
@@ -1399,7 +1428,13 @@ function ReforgeLite:CreateOptionList ()
       end,
       width = 110,
       menuItemDisabled = function(val)
-        return val > 0 and self.statCaps[3-i].stat.value == val
+        if val > 0 then
+          for k,v in ipairs(self.statCaps) do
+            if k~=i and v.stat.value == val then
+              return true
+            end
+          end
+        end
       end
     })
     self.statCaps[i].add = GUI:CreateImageButton (self.statCaps, 20, 20, "Interface\\Buttons\\UI-PlusButton-Up",
@@ -1410,7 +1445,7 @@ function ReforgeLite:CreateOptionList ()
     self.statCaps:SetCell (i, 0, self.statCaps[i].stat, "LEFT", -20, -10)
     self.statCaps:SetCell (i, 2, self.statCaps[i].add, "LEFT")
   end
-  for i = 1, 2 do
+  for i = 1, 3 do
     for point in ipairs(self.pdb.caps[i].points) do
       self:AddCapPoint (i, point)
     end
@@ -1421,7 +1456,7 @@ function ReforgeLite:CreateOptionList ()
   end
   self.statCaps.onUpdate = function ()
     local row = 1
-    for i = 1, 2 do
+    for i = 1, 3 do
       row = row + 1
       for point = 1, #self.pdb.caps[i].points do
         if self.statCaps.cells[row][2] and self.statCaps.cells[row][2].values then
