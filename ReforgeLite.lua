30fa4ff30666f20d5a2af75f2d2ba38ba65c49b7
skyler-code <skylerh15@github.com>
2024-08-17 23:43:49 -0500

    Dont recommend spirit-to-hit for non-ele/boomkin
    This reverts commit 0381a078799066a93eb36cbeb9128698c9956636.

--- ReforgeLite.lua
+++ ReforgeLite.lua
@@ -1966,10 +1966,10 @@ function ReforgeLite:UpdateItems()
   if playerClass == "PRIEST" then
     local pts = select(5, GetTalentInfo (3, 20))
     self.s2hFactor = pts * 50
-  elseif playerClass == "DRUID" then
+  elseif playerClass == "DRUID" and GetPrimaryTalentTree () ~= 2 then
     local pts = select(5, GetTalentInfo (1, 7))
     self.s2hFactor = pts * 50
-  elseif playerClass == "SHAMAN" then
+  elseif playerClass == "SHAMAN" and GetPrimaryTalentTree () ~= 2 then
     local pts = select(5, GetTalentInfo (1, 9))
     self.s2hFactor = (pts == 3 and 100 or pts * 33)
   elseif playerClass == "PALADIN" then
