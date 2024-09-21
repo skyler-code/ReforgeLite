b093a589d2cd4d9b611ef4853bd45ac70ef170dd
skyler-code <skylerhiscock@gmail.com>
2024-09-21 18:15:09 -0500

    extract table funcs to util file

--- ReforgeLite.lua
+++ ReforgeLite.lua
@@ -11,27 +11,8 @@ local GUI = addonTable.GUI
 local LibDD = LibStub:GetLibrary("LibUIDropDownMenu-4.0")
 addonTable.MAX_LOOPS = 125000
 
-local function DeepCopy (t, cache)
-  if type (t) ~= "table" then
-    return t
-  end
-  local copy = {}
-  for i, v in pairs (t) do
-    if type (v) ~= "table" then
-      copy[i] = v
-    else
-      cache = cache or {}
-      cache[t] = copy
-      if cache[v] then
-        copy[i] = cache[v]
-      else
-        copy[i] = DeepCopy (v, cache)
-      end
-    end
-  end
-  return copy
-end
-addonTable.DeepCopy = DeepCopy
+local MergeTables = addonTable.MergeTables
+local DeepCopy = addonTable.DeepCopy
 
 local gprint = print
 local function print(...)
@@ -106,21 +87,6 @@ local DefaultDBProfile = {
 local DefaultDBClassProfile = {
   customPresets = {}
 }
-local function MergeTables (dst, src)
-  for k, v in pairs (src) do
-    if type (v) ~= "table" then
-      if dst[k] == nil then
-        dst[k] = v
-      end
-    else
-      if type (dst[k]) ~= "table" then
-        dst[k] = {}
-      end
-      MergeTables (dst[k], v)
-    end
-  end
-end
-addonTable.MergeTables = MergeTables
 
 local function ReforgeFrameIsVisible()
   return ReforgingFrame and ReforgingFrame:IsShown()
